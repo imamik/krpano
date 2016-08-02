97c47b22b91ded59dfa8ceb14a03bf6b89bad59c
Devin Chen <d3vin.chen@gmail.com>
2016-08-02 19:40:54 +0800

    fix testing script

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 TIMEOUT = 15000
 test:
 	./node_modules/.bin/mocha -t $(TIMEOUT)
+#	./node_modules/.bin/mocha -t $(TIMEOUT) --debug-brk 
 
 cov test-cov:
 	./node_modules/.bin/istanbul cover _mocha -- -t $(TIMEOUT)
--- test/krpano.test.js
+++ test/krpano.test.js
@@ -5,22 +5,22 @@ const config = {
 };
 
 var krpano = require('../').create(config);
-var krpano2 = require('../').create(config);
+//var krpano2 = require('../').create(config);
 
 describe('test/krpano.test.js',function () {
-  describe('make from internet ',function () {
-    it('should have result',function (done) {
-      krpano.makepano({
-        url: 'http://www.xuanran001.com/public/repository/708a/6be2/40ca/4f9f/b05c/f622/5eda/fca1/image/0000.png'
-      }).then(function (result) {
-        result.should.be.an.instanceOf(Object);
-        done();
-      });
-    });
-  });
-  describe('make from local url  ',function () {
+  //describe('make from internet ',function () {
+  //  it('should have result',function (done) {
+  //    krpano.makepano({
+  //      url: 'http://www.xuanran001.com/public/repository/708a/6be2/40ca/4f9f/b05c/f622/5eda/fca1/image/0000.png'
+  //    }).then(function (result) {
+  //      result.should.be.an.instanceOf(Object);
+  //      done();
+  //    });
+  //  });
+  //});
+  describe('make from local url', function () {
     it('should return object',function (done) {
-      ocr.scan({
+      krpano.makepano({
         url: __dirname+'/test.jpg'
       }).then(function(result) {
         result.should.be.an.instanceOf(Object);
